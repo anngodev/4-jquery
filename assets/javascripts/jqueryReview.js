5e2bf9a888b864d878feec80d42ae88bb0b0c525
An Ngo <ngoan_230@yahoo.com>
2017-05-13 05:04:44 -0400

    click on square

--- assets/javascripts/jqueryReview.js
+++ assets/javascripts/jqueryReview.js
@@ -1,4 +1,13 @@
 // this is equivalent to document.getElementById
 $("#circle").click(function() {
-	alert("Circle was clicked!")
+	alert("Circle was clicked!");
 });
+
+$(".square").click(function() {
+	alert("Square was clicked!");
+});
+
+// can able to target divs
+// $("div").click(function() {
+// 	alert("Square was clicked!");
+// });
