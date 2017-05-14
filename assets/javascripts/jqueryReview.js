40b596a5da986f3ed5fa1424f2888ef6f64aff1e
An Ngo <ngoan_230@yahoo.com>
2017-05-14 06:16:02 -0400

    jquery fadeOut boolean

--- assets/javascripts/jqueryReview.js
+++ assets/javascripts/jqueryReview.js
@@ -10,11 +10,36 @@
 
 
 // "this" is a keyword in this particular case is referring to the element that we are dealing with at the moment. Enabling a single function applying to different number of elements depending on the action specified (for example: click, hover, etc.)
-$("div").click(function() {
-	if ($(this).attr("id") == "circle") {
-		alert("You clicked on a circle");
+// $("div").click(function() {
+// 	if ($(this).attr("id") == "circle") {
+// 		alert("You clicked on a circle");
 
+// 	} else {
+// 		alert("You clicked on a square")
+// 	}
+// });
+
+// $("div").click(function() {
+// 	$(this).fadeOut("slow", function() {
+// 		alert("fadeout has finished")
+// 	});
+// });
+
+$("#fadeOut").click(function() {
+	$("#oldText").fadeOut("slow");
+});
+
+$("#fadeIn").click(function() {
+	$("#newText").fadeIn("slow");
+});
+
+$("#toggle").click(function() {
+	if ($("#toggleText").css("display") == "none") {
+		$("#toggleText").fadeIn("slow")
 	} else {
-		alert("You clicked on a square")
+		$("#toggleText").fadeOut("slow");
 	}
 });
+
+
+
