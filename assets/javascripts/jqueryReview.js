869899e7ad6d5daf1af8892b98dc795042a4259e
An Ngo <ngoan_230@yahoo.com>
2017-05-14 05:14:11 -0400

    jquery this keyword

--- assets/javascripts/jqueryReview.js
+++ assets/javascripts/jqueryReview.js
@@ -1,9 +1,20 @@
 // this is equivalent to document.getElementById
-$("#circle").hover(function() {
-	$("iframe").attr("src", "http://www.foxnews.com");
-});
+// $("#circle").hover(function() {
+// 	$("iframe").attr("src", "http://www.foxnews.com");
+// });
 
 // can able to target divs
 // $("div").click(function() {
 // 	alert("Square was clicked!");
 // });
+
+
+// "this" is a keyword in this particular case is referring to the element that we are dealing with at the moment. Enabling a single function applying to different number of elements depending on the action specified (for example: click, hover, etc.)
+$("div").click(function() {
+	if ($(this).attr("id") == "circle") {
+		alert("You clicked on a circle");
+
+	} else {
+		alert("You clicked on a square")
+	}
+});
