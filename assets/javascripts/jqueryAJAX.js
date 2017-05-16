a9d1f4632895aa6702df8b9f50114361899c93ac
An Ngo <ngoan_230@yahoo.com>
2017-05-16 06:03:24 -0400

    jquery regex

--- assets/javascripts/jqueryAJAX.js
+++ assets/javascripts/jqueryAJAX.js
@@ -9,5 +9,17 @@ $.ajax("info.txt")
 
 	})
 		.fail(function() {
-		alert("Could not get data")
+		alert("Could not get data");
 	});
+
+// a regular expression are to check or format a content of a string
+
+var regex = /Great/i;
+
+	var string = "Regex is great!";
+
+	// this variable is saying match the string "Regex is great!" according to the regex variable ie /is/
+	// what this means is that it is searching if the word "is" is in the string var.
+	var result = string.match(regex);
+
+	alert(result);
